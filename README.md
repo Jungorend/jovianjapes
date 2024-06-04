7f9f7e55371e95ae3a205ee6f81163bb36f21f74
mweber <mweber@tenable.com>
2024-06-04 10:22:02 -0400

    Better Readme, hashtable creation

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # Liminality
 
-The threshold between two forms
+Liminality: the gateway between two stages.
+
+## Goals
+This dungeon crawler is about transition, and what is gained and lost in the process.
+It aims to make battles as engaging the 1000th time as the 100th.
+
+But short term the goals are:
+- To actually exist
+- Functional game loop that runs at a reasonable pace
+- Full level editor that can be understood by anyone.
+- The modest game assets achievable by a non-artist.
+
+## Current State
+
+3D world is walkable, and movement is animated over time. ECS system and in-game debugger made.
 
 ## Requirements
 
@@ -18,3 +32,10 @@ Pull this repository to your quicklisp/local-projects directory. Then run the fo
 (main)
 (load-level "level-one")
 ```
+
+## Todo
+
+- Write raylib bindings for importing and manipulating 3d models
+- Adjust the ECS system to use events as a queue rather than the byzantine timer system I thought was a good idea.
+- Transition system for allowing switching to battles/menus(?)
+- start to actually design the game, not the engine.
--- raylib.lisp
+++ raylib.lisp
@@ -6,32 +6,32 @@
 (fli:define-c-typedef bool (:boolean :byte))
 
 (fli:define-c-struct color-struct
-  (r :byte) (g :byte) (b :byte) (a :byte))
+    (r :byte) (g :byte) (b :byte) (a :byte))
 
 (fli:define-c-struct vector2
-  (x :float) (y :float))
+    (x :float) (y :float))
 
 (fli:define-c-struct vector3
-  (x :float) (y :float) (z :float))
+    (x :float) (y :float) (z :float))
 
 (fli:define-c-struct vector4
-  (x :float) (y :float) (z :float) (w :float))
+    (x :float) (y :float) (z :float) (w :float))
 
 (fli:define-c-struct camera3D
-  (position vector3)
+    (position vector3)
   (target vector3)
   (up vector3)
   (fovy :float)
   (projection :int))
 
 (fli:define-c-struct matrix
-  (m0 :float) (m4 :float) (m8 :float) (m12 :float)
+    (m0 :float) (m4 :float) (m8 :float) (m12 :float)
   (m1 :float) (m5 :float) (m9 :float) (m13 :float)
   (m2 :float) (m6 :float) (m10 :float) (m14 :float)
   (m3 :float) (m7 :float) (m11 :float) (m15 :float))
 
 (fli:define-c-struct mesh
-  (vertex-count :int)
+    (vertex-count :int)
   (triangle-count :int)
   (vertices (:pointer :float))
   (texcoords (:pointer :float))
@@ -50,7 +50,7 @@
   (vbo-id (:unsigned :int)))
 
 (fli:define-c-struct model        ; NOTE: None of these are actually expanded on
-  (transform (:struct matrix))
+    (transform (:struct matrix))
   (mesh-count :int)
   (material-count :int)
   (meshes :pointer)
@@ -61,18 +61,18 @@
 
 
 (fli:define-foreign-function (load-model "LoadModel")
-  ((file-name (:reference-pass :ef-mb-string)))
+    ((file-name (:reference-pass :ef-mb-string)))
   :result-type (:struct model)
   :result-pointer model
   :documentation "Load model from files (meshes and materials)")
 
 (fli:define-foreign-function (model-ready? "IsModelReady")
-  ((model (:struct model)))
+    ((model (:struct model)))
   :result-type :boolean
   :documentation "Check if a model is ready")
 
 (fli:define-foreign-function (unload-model "UnloadModel")
-  ((model (:struct model)))
+    ((model (:struct model)))
   :documentation "Unload model (including mesh) from memory (RAM and/or VRAM)")
 
 (fli:define-foreign-function (%get-camera-matrix "GetCameraMatrix")
@@ -140,7 +140,7 @@
   :documentation "Get current screen height")
 
 (fli:define-foreign-function (get-time "GetTime")
-  ()
+    ()
   :result-type :double-float
   :documentation "Elapsed time in seconds since InitWindow()")
 
@@ -166,10 +166,10 @@
 
 (defun draw-cube (position width height length color)
   (fli:with-dynamic-foreign-objects
-    ((pos vector3)
-     (col color-struct))
+      ((pos vector3)
+       (col color-struct))
     (let ((color-values (gethash color +colors+)))
-        (setf (fli:foreign-slot-value pos 'x) (nth 0 position)
+      (setf (fli:foreign-slot-value pos 'x) (nth 0 position)
             (fli:foreign-slot-value pos 'y) (nth 1 position)
             (fli:foreign-slot-value pos 'z) (nth 2 position)
             (fli:foreign-slot-value col 'r) (nth 0 color-values)
@@ -258,63 +258,62 @@
             (fli:foreign-slot-value cam 'projection) (projection camera))
       (%begin-mode-3d cam))))
 
-(defun build-hash-table (table values)
-  (loop for entry in values
-        do (setf (gethash (first entry) table) (second entry))))
+(defmacro build-hash-table (table &rest values &key (test 'eql) &allow-other-keys)
+  (let ((entry (gensym)))
+    `(progn
+       (setf ,table (make-hash-table :test ,test))
+       (dolist (,entry ',values)
+         (setf (gethash (first ,entry) ,table)
+               (second ,entry))))))
 
-(defparameter +colors+ (make-hash-table))
-(setf (gethash 'lightgray +colors+) '(200 200 200 255)
-      (gethash 'gray +colors+) '(130 130 130 255)
-      (gethash 'darkgray +colors+) '(80 80 80 255)
-      (gethash 'yellow +colors+) '(253 249 0 255)
-      (gethash 'gold +colors+) '(255 203 0 255)
-      (gethash 'orange +colors+) '(255 161 0 255)
-      (gethash 'pink +colors+) '(255 109 194 255)
-      (gethash 'red +colors+) '(230 41 55 255)
-      (gethash 'maroon +colors+) '(190 33 55 255)
-      (gethash 'green +colors+) '(0 228 48 255)
-      (gethash 'lime +colors+) '(0 158 47 255)
-      (gethash 'darkgreen +colors+) '(0 117 44 255)
-      (gethash 'skyblue +colors+) '(102 191 255 255)
-      (gethash 'blue +colors+) '(0 121 241 255)
-      (gethash 'darkblue +colors+) '(0 82 172 255)
-      (gethash 'purple +colors+) '(112 31 126 255)
-      (gethash 'violet +colors+) '(135 60 190 255)
-      (gethash 'darkpurple +colors+) '(112 31 126 255)
-      (gethash 'beige +colors+) '(211 176 131 255)
-      (gethash 'brown +colors+) '(127 106 79 255)
-      (gethash 'darkbrown +colors+) '(76 63 47 255)
-      (gethash 'white +colors+) '(255 255 255 255)
-      (gethash 'black +colors+) '(0 0 0 255)
-      (gethash 'blank +colors+) '(0 0 0 0)
-      (gethash 'magenta +colors+) '(255 0 255 255)
-      (gethash 'raywhite +colors+) '(245 245 245 255))
+(build-hash-table +colors+
+                  (lightgray (200 200 200 255))
+                  (gray (130 130 130 255))
+                  (darkgray (80 80 80 255))
+                  (yellow (253 249 0 255))
+                  (gold (255 203 0 255))
+                  (orange (255 161 0 255))
+                  (pink (255 109 194 255))
+                  (red (230 41 55 255))
+                  (maroon (190 33 55 255))
+                  (green (0 228 48 255))
+                  (lime (0 158 47 255))
+                  (darkgreen (0 117 44 255))
+                  (skyblue (102 191 255 255))
+                  (blue (0 121 241 255))
+                  (darkblue (0 82 172 255))
+                  (purple (112 31 126 255))
+                  (violet (135 60 190 255))
+                  (darkpurple (112 31 126 255))
+                  (white (255 255 255 255))
+                  (black (0 0 0 255))
+                  (blank (0 0 0 0))
+                  (magenta (255 0 255 255))
+                  (raywhite (245 245 245 255)))
 
-(defparameter +camera-modes+ (make-hash-table))
 (build-hash-table +camera-modes+
-                  '((camera-custom 0)
-                    (camera-free 1)
-                    (camera-orbital 2)
-                    (camera-first-person 3)
-                    (camera-third-person 4)))
+                  (camera-custom 0)
+                  (camera-free 1)
+                  (camera-orbital 2)
+                  (camera-first-person 3)
+                  (camera-third-person 4))
 
-(defparameter +input-codes+ (make-hash-table))
 (build-hash-table +input-codes+
-                  '((key-null 0)
-                    (key-apostrophe 39) (key-comma 44) (key-minus 45) (key-period 46) (key-slash 47)
-                    (key-zero 48) (key-one 49) (key-two 50) (key-three 51) (key-four 52) (key-five 53) (key-six 54) (key-seven 55) (key-eight 56) (key-nine 57)
-                    (key-semicolon 59) (key-equal 61)
-                    (key-a 65) (key-b 66) (key-c 67) (key-d 68) (key-e 69) (key-f 70) (key-g 71) (key-h 72) (key-i 73) (key-j 74) (key-k 75) (key-l 76) (key-m 77)
-                    (key-n 78) (key-o 79) (key-p 80) (key-q 81) (key-r 82) (key-s 83) (key-t 84) (key-u 85) (key-v 86) (key-w 87) (key-x 88) (key-y 89) (key-z 90)
-                    (key-left-bracket 91) (key-backslash 92) (key-right-bracket 93) (key-grave 96) (key-space 32) (key-escape 256) (key-enter 257) (key-tab 258)
-                    (key-backspace 259) (key-insert 260) (key-delete 261)
-                    (key-right 262) (key-left 263) (key-down 264) (key-up 265) (key-page-up 266) (key-page-down 267) (key-home 268) (key-end 269)
-                    (key-caps-lock 280) (key-scroll-lock 281) (key-num-lock 282) (key-print-screen 283) (key-pause 284)
-                    (key-f1 290) (key-f2 291) (key-f3 292) (key-f4 293) (key-f5 294) (key-f6 295) (key-f7 296) (key-f8 297) (key-f9 298) (key-f10 299) (key-f11 300) (key-f12 301)
-                    (key-left-shift 340) (key-left-control 341) (key-left-alt 342) (key-left-super 343) (key-right-shift 344) (key-right-control 345) (key-right-alt 346) (key-right-super 347) (key-kb-menu 348)
-                    (key-kp-0 320) (key-kp-1 321) (key-kp-2 322) (key-kp-3 323) (key-kp-4 324) (key-kp-5 325) (key-kp-6 326) (key-kp-7 327) (key-kp-8 328) (key-kp-9 329)
-                    (key-kp-decimal 330) (key-kp-divide 331) (key-kp-multiply 332) (key-kp-subtract 333) (key-kp-add 334) (key-kp-enter 335) (key-kp-equal 336)
-                    (key-back 4) (key-menu 5) (key-volume-up 24) (key-volume-down 25)))
+                  (key-null 0)
+                  (key-apostrophe 39) (key-comma 44) (key-minus 45) (key-period 46) (key-slash 47)
+                  (key-zero 48) (key-one 49) (key-two 50) (key-three 51) (key-four 52) (key-five 53) (key-six 54) (key-seven 55) (key-eight 56) (key-nine 57)
+                  (key-semicolon 59) (key-equal 61)
+                  (key-a 65) (key-b 66) (key-c 67) (key-d 68) (key-e 69) (key-f 70) (key-g 71) (key-h 72) (key-i 73) (key-j 74) (key-k 75) (key-l 76) (key-m 77)
+                  (key-n 78) (key-o 79) (key-p 80) (key-q 81) (key-r 82) (key-s 83) (key-t 84) (key-u 85) (key-v 86) (key-w 87) (key-x 88) (key-y 89) (key-z 90)
+                  (key-left-bracket 91) (key-backslash 92) (key-right-bracket 93) (key-grave 96) (key-space 32) (key-escape 256) (key-enter 257) (key-tab 258)
+                  (key-backspace 259) (key-insert 260) (key-delete 261)
+                  (key-right 262) (key-left 263) (key-down 264) (key-up 265) (key-page-up 266) (key-page-down 267) (key-home 268) (key-end 269)
+                  (key-caps-lock 280) (key-scroll-lock 281) (key-num-lock 282) (key-print-screen 283) (key-pause 284)
+                  (key-f1 290) (key-f2 291) (key-f3 292) (key-f4 293) (key-f5 294) (key-f6 295) (key-f7 296) (key-f8 297) (key-f9 298) (key-f10 299) (key-f11 300) (key-f12 301)
+                  (key-left-shift 340) (key-left-control 341) (key-left-alt 342) (key-left-super 343) (key-right-shift 344) (key-right-control 345) (key-right-alt 346) (key-right-super 347) (key-kb-menu 348)
+                  (key-kp-0 320) (key-kp-1 321) (key-kp-2 322) (key-kp-3 323) (key-kp-4 324) (key-kp-5 325) (key-kp-6 326) (key-kp-7 327) (key-kp-8 328) (key-kp-9 329)
+                  (key-kp-decimal 330) (key-kp-divide 331) (key-kp-multiply 332) (key-kp-subtract 333) (key-kp-add 334) (key-kp-enter 335) (key-kp-equal 336)
+                  (key-back 4) (key-menu 5) (key-volume-up 24) (key-volume-down 25))
 
 (defun key-code (code)
   (gethash code +input-codes+))
